e7ff06ca86e57055bbfc43e654bddd758bc62659
NikKrasavchik <stachinskiy.nikita@gmail.com>
2023-10-23 13:44:39 +0300

    first final

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nikkk/Documents/try/phonotry/engine/conf.cpp" "CMakeFiles/app.dir/engine/conf.cpp.o" "gcc" "CMakeFiles/app.dir/engine/conf.cpp.o.d"
+  "/Users/nikkk/Documents/try/phonotry/engine/letter.cpp" "CMakeFiles/app.dir/engine/letter.cpp.o" "gcc" "CMakeFiles/app.dir/engine/letter.cpp.o.d"
+  "/Users/nikkk/Documents/try/phonotry/engine/phonotext.cpp" "CMakeFiles/app.dir/engine/phonotext.cpp.o" "gcc" "CMakeFiles/app.dir/engine/phonotext.cpp.o.d"
+  "/Users/nikkk/Documents/try/phonotry/engine/proccessing.cpp" "CMakeFiles/app.dir/engine/proccessing.cpp.o" "gcc" "CMakeFiles/app.dir/engine/proccessing.cpp.o.d"
+  "/Users/nikkk/Documents/try/phonotry/phonotry.cpp" "CMakeFiles/app.dir/phonotry.cpp.o" "gcc" "CMakeFiles/app.dir/phonotry.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
