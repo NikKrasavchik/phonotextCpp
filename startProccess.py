97a8754c8f44d7884dc6ac27d3c7aab4cdc81130
NikKrasavchik <stachinskiy.nikita@gmail.com>
2024-03-19 14:31:05 +0300

    may be full automatic

--- startProccess.py
+++ startProccess.py
@@ -3,4 +3,30 @@ import os
 textNames = os.listdir('../texts/')
 
 for textName in textNames:
+    if textName[0] == '.': continue
+    
+    if textName[-4::] == '.txt':
+        os.rename('../texts/' + textName, '../texts/' + textName[:-4:])
+        textName = textName[:-4:]
+
+    print(textName)
+
+    try:
+        f = open("../texts/" + textName, encoding="utf-8")
+        f.read()
+    except:
+        os.rename('../texts/' + textName, '../texts/' + textName+'.txt')
+        continue
+        os.system(f'iconv -f 1251 -t utf8 ../texts/{textName} | tee {textName}')
+    f = open("../texts/" + textName, encoding="utf-8")
+    lines = f.readlines()
+    f.close()
+    f = open("../texts/" + textName, 'w', encoding="utf-8")
+    for i in range(len(lines)):
+        str = ""
+        for j in range(len(lines[i])):
+            str += lines[i][j].lower()
+        f.write(str)
+    f.close()
+
     os.system('/Users/nikkk/Documents/phonotextCpp/build/app \"' + textName + '\"')
--- tolower.py
+++ tolower.py
@@ -1,11 +1,16 @@
-f = open("texts/Алине _ Александр Бестужев")
-lines = f.readlines()
-f.close()
-f = open("texts/Алине _ Александр Бестужев", 'w')
+import os
 
-for i in range(len(lines)):
-  str = ""
-  for j in range(len(lines[i])):
-    str += lines[i][j].lower()
-  f.write(str)
-f.close()
+textNames = os.listdir('../texts/')
+
+for textName in textNames:
+    f = open("../texts/" + textName, encoding="utf8")
+
+    lines = f.readlines()
+    f.close()
+    f = open("../texts/" + textName, 'w')
+    for i in range(len(lines)):
+      str = ""
+      for j in range(len(lines[i])):
+        str += lines[i][j].lower()
+      f.write(str)
+    f.close()
